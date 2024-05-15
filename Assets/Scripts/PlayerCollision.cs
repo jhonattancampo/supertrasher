06da93ff6596d55c68716d1c48b8252ad3b3120b
Jhonattan Campo <awesomejohnny@192.168.1.8>
2024-05-15 15:59:28 -0300

    Ciclo completo

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-   private void OnCollisionEnter2D(Collision2D other) {
-     Debug.Log("Tag: " + other.transform.tag);
-    if(other.transform.tag == "Obstaculo") {
-        Destroy(gameObject);
-        GameManager.Instance.GameOver();
+    private void Start()
+    {
+        GameManager.Instance.onPlay.AddListener(ActivatePlayer);
+        GameManager.Instance.onGameOver.AddListener(DeactivatePlayer);
     }
-   }
+
+    private void ActivatePlayer()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void DeactivatePlayer()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag == "Obstaculo")
+        {
+            // Use the AdjustScore method to decrement the score
+            GameManager.Instance.AdjustScore(-1);
+        }
+        else if (other.transform.tag == "Trash")
+        {
+            // Use the AdjustScore method to increment the score
+            GameManager.Instance.AdjustScore(1);
+        }
+    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,11 +6,22 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI ScoreUI;
-    
+    [SerializeField] private GameObject startMenuUI;
+    [SerializeField] private GameObject gameOverUI;
     GameManager gm;
 
     private void Start() {
         gm = GameManager.Instance;
+        gm.onGameOver.AddListener(ActivateGameOverUI);
+    }
+
+    public void PlayButtonHandler() {
+        gm.StartGame();
+        startMenuUI.SetActive(false);
+    }
+
+    public void ActivateGameOverUI() {
+        gameOverUI.SetActive(true);
     }
 
     private void OnGUI() {
